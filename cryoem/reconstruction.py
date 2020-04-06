9fc7117221426321c346bcff49c474e1a0b0d25d
Jelena Banjac <jelena.b94@gmail.com>
2020-04-06 15:19:53 +0200

    add some reconstructions

--- cryoem/reconstruction.py
+++ cryoem/reconstruction.py
@@ -6,45 +6,42 @@ from cryoem.projections import RotationMatrix
 import astra
 
 
-def RotationMatrix(angles):
-    vectors = np.zeros((angles.shape[0],12))
-    vectors[:,0:3] = [0, 0, 1]
+# def RotationMatrix(angles):
+#     vectors = np.zeros((angles.shape[0],12))
+#     vectors[:,0:3] = [0, 0, 1]
 
-    # center of detector
-    vectors[:,3:6] = 0
+#     # center of detector
+#     vectors[:,3:6] = 0
  
-    # vector from detector pixel (0,0) to (0,1)
-    vectors[:,6:9] = [1, 0, 0]
+#     # vector from detector pixel (0,0) to (0,1)
+#     vectors[:,6:9] = [1, 0, 0]
      
-    # vector from detector pixel (0,0) to (1,0)
-    vectors[:,9:12]  = [0, 1, 0]
+#     # vector from detector pixel (0,0) to (1,0)
+#     vectors[:,9:12]  = [0, 1, 0]
  
-    # create rotation matrix
-    c1 = np.cos(angles[:,0]).reshape(-1,1,1)
-    c2 = np.cos(angles[:,1]).reshape(-1,1,1)
-    c3 = np.cos(angles[:,2]).reshape(-1,1,1)
+#     # create rotation matrix
+#     c1 = np.cos(angles[:,0]).reshape(-1,1,1)
+#     c2 = np.cos(angles[:,1]).reshape(-1,1,1)
+#     c3 = np.cos(angles[:,2]).reshape(-1,1,1)
                         
-    s1 = np.sin(angles[:,0]).reshape(-1,1,1)
-    s2 = np.sin(angles[:,1]).reshape(-1,1,1)
-    s3 = np.sin(angles[:,2]).reshape(-1,1,1)
-    vector = vectors[0,:]
+#     s1 = np.sin(angles[:,0]).reshape(-1,1,1)
+#     s2 = np.sin(angles[:,1]).reshape(-1,1,1)
+#     s3 = np.sin(angles[:,2]).reshape(-1,1,1)
+#     vector = vectors[0,:]
 
-    R = np.concatenate([np.concatenate([c1*c2*c3-s1*s3, c1*s3+c2*c3*s1 , -c3*s2],axis=2),\
-                        np.concatenate([-c3*s1-c1*c2*s3,    c1*c3-c2*s1*s3 ,   s2*s3],axis=2),\
-                        np.concatenate( [c1*s2,             s1*s2          ,   c2],axis=2)],axis=1)
-    #R = np.transpose(R, (0, 2, 1))
+#     R = np.concatenate([np.concatenate([c1*c2*c3-s1*s3, c1*s3+c2*c3*s1 , -c3*s2],axis=2),\
+#                         np.concatenate([-c3*s1-c1*c2*s3,    c1*c3-c2*s1*s3 ,   s2*s3],axis=2),\
+#                         np.concatenate( [c1*s2,             s1*s2          ,   c2],axis=2)],axis=1)
+#     #R = np.transpose(R, (0, 2, 1))
     
-    # rotate previous values
-    vectors[:,0:3] = np.matmul(R,vector[0:3])
-    vectors[:,6:9] = np.matmul(R,vector[6:9])
-    vectors[:,9:12] = np.matmul(R,vector[9:12])
+#     # rotate previous values
+#     vectors[:,0:3] = np.matmul(R,vector[0:3])
+#     vectors[:,6:9] = np.matmul(R,vector[6:9])
+#     vectors[:,9:12] = np.matmul(R,vector[9:12])
 
-    return vectors
+#     return vectors
 
-def reconstruct(input_file, output_file, save_mrc=True):
-    data = np.load(f'data/{input_file}.npz')
-    projections, angles = data["arr_0"].astype(np.float64)[:3000, :, :], data["arr_1"].astype(np.float64)[:3000, :]
-
+def reconstruct(projections, angles, mrc_filename=True):
     # Generate orientation vectors based on angles
     orientation_vectors   = RotationMatrix(angles)
 
@@ -80,8 +77,14 @@ def reconstruct(input_file, output_file, save_mrc=True):
     astra.data3d.delete(projections_id)
 
     # Save reconstruction to mrc file for chimera
-    if save_mrc:
-        with mrcfile.new(f'data/{output_file}.mrc') as mrc:
+    if mrc_filename:
+        with mrcfile.new(mrc_filename) as mrc:
             mrc.set_data(reconstruction)
         
     return reconstruction
+
+def reconstruct_from_file(input_file, limit=3000, mrc_filename=None):
+    data = np.load(f'data/{input_file}.npz')
+    projections, angles = data["arr_0"].astype(np.float64)[:limit, :, :], data["arr_1"].astype(np.float64)[:limit, :]
+
+    return reconstruct(projections, angles, mrc_filename)
